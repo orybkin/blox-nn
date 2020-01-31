9d0503080235d6db8a560f691b8e161c81ba77cd
Oleh Rybkin <oleh.rybkin@gmail.com>
2020-01-31 00:20:45 -0500

    safe attrdict access

--- core.py
+++ core.py
@@ -23,6 +23,29 @@ class AttrDict(dict):
     def __getstate__(self): return self
     def __setstate__(self, d): self = d
 
+    @property
+    def safe(self):
+        """
+        A property that can be used to safely access the dictionary.
+        When a key is not found, the access getter will return None.
+
+        To be used as:
+        AttrDict().safe.keypoint == None
+        """
+        return DictAccess(self)
+    
+
+class DictAccess():
+    # A simple access class that returns None if a key is not found
+    def __init__(self, parent):
+        super().__setattr__('parent', parent)
+    
+    def __getattr__(self, item):
+        try:
+            return dict.__getitem__(self.parent, item)
+        except KeyError:
+            return None
+    
 
 class optional:
     """ A function decorator that returns the first argument to the function if yes=False
