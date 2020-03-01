61bdb287d5c679bb23f1845503a3113da8bf506b
Oleh Rybkin <oleh.rybkin@gmail.com>
2020-02-29 22:07:12 -0500

    restore the very original VRNN as VRNN1

--- torch/models/vrnn.py
+++ torch/models/vrnn.py
@@ -166,3 +166,29 @@ class VRNN(nn.Module):
             (model_output.q_z, model_output.p_z, reduction=[-1, -2], log_error_arr=log_error_arr)
         
         return losses
+
+
+class VRNN1(VRNN):
+    def forward(self, x, output_length, conditioning_length, context=None):
+        """
+
+        :param x: the modelled sequence, batch x time x  x_dim
+        :param length: the desired length of the output sequence. Note, this includes all conditioning frames except 1
+        :param conditioning_length: the length on which the prediction will be conditioned. Ground truth data are observed
+        for this length
+        :param context: a context sequence. Prediction is conditioned on all context up to and including this moment
+        :return:
+        """
+        lstm_inputs = AttrDict(x_prime=x[:, 1:])
+        if context is not None:
+            context = pad(context, pad_front=1, dim=1)
+            lstm_inputs.update(more_context=context[:, 1:])
+        
+        initial_inputs = AttrDict(x=x[:, :conditioning_length])
+        
+        self.lstm.cell.init_state(x[:, 0], more_context=context)
+        outputs = self.lstm(inputs=lstm_inputs, initial_seq_inputs=initial_inputs,
+                            length=output_length + conditioning_length - 1)
+        outputs = rmap(lambda ten: ten[:, conditioning_length - 1:], outputs)
+        return outputs
+    
