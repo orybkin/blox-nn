normalization fix with KL divergence losses

torch/models/vrnn.py
@@ -158,6 +158,6 @@ class VRNN(nn.Module):
         losses = AttrDict()
         
         losses.kl = KLDivLoss(self._hp.kl_weight) \
-            (model_output.q_z, model_output.p_z, reduction=[-1, -2], log_error_arr=log_error_arr)
+            (model_output.q_z, model_output.p_z, reduction=[-1, -2, -3, -4], log_error_arr=log_error_arr)
         
         return losses

torch/variational.py
@@ -106,7 +106,7 @@ class AttentiveInference(nn.Module):
         if q_z.mu.numel() == 0:
             return {}
         
-        return AttrDict(kl=KLDivLoss(self._hp.kl_weight, breakdown=1)(q_z, p_z, log_error_arr=True, reduction=[-1, -2]))
+        return AttrDict(kl=KLDivLoss(self._hp.kl_weight, breakdown=1)(q_z, p_z, log_error_arr=True, reduction=[-1, -2, -3, -4]))
     
     def get_dummy(self, e_l):
         raise NotImplementedError('do we need to run inference in this case?')