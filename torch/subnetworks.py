c79d494db3e51ed4a58cff5c4fff99d0d151c89c
Oleh Rybkin <oleh.rybkin@gmail.com>
2020-04-15 22:34:05 -0400

    updated attention to appropriately tile the inputs for GCP

--- torch/subnetworks.py
+++ torch/subnetworks.py
@@ -296,7 +296,23 @@ class Attention(nn.Module):
         self.out = nn.Linear(hp.nz_enc, hp.nz_enc)
 
     def forward(self, values, keys, query_input, start_ind, end_ind, inputs, timestep=None, attention_weights=None):
-        """Performs multi-layered, multi-headed attention."""
+        """
+        Performs multi-layered, multi-headed attention.
+
+        Note: the query can have a different batch size from the values/keys. In that case, the query is interpreted as
+        multiple queries, i.e. the values are tiled to match the query tensor size.
+        
+        :param values: tensor batch x length x dim_v
+        :param keys: tensor batch x length x dim_k
+        :param query_input: input to the query network, batch2 x dim_k
+        :param start_ind:
+        :param end_ind:
+        :param inputs:
+        :param timestep: specify the timestep of the attention directly. tensor batch2 x 1
+        :param attention_weights:
+        :return:
+        """
+        
         if timestep is not None:
             mult = int(timestep.shape[0] / keys.shape[0])
             if mult > 1:
@@ -305,14 +321,20 @@ class Attention(nn.Module):
                 return result.reshape([-1] + list(result.shape[2:])), None
                 
             return batchwise_index(values, timestep[:, 0].long()), None
-        
-        raise NotImplementedError('This us not updated to the fact the values and keys are no longer appropriately tiled')
-        
+
         query = self.query_net(*query_input)
-        
-        # Attend
         s_ind, e_ind = (torch.floor(start_ind), torch.ceil(end_ind)) if self._hp.mask_inf_attention \
                                                                      else (inputs.start_ind, inputs.end_ind)
+        
+        # Reshape values, keys, inputs if not enough dimensions
+        mult = int(query.shape[0] / keys.shape[0])
+        tile = lambda x: x[:, None][:, [0] * mult].reshape((-1,) + x.shape[1:])
+        values = tile(values)
+        keys = tile(keys)
+        s_ind = tile(s_ind)
+        e_ind = tile(e_ind)
+        
+        # Attend
         norm_shape_k = query.shape[1:]
         norm_shape_v = values.shape[2:]
         raw_attn_output, att_weights = None, None
